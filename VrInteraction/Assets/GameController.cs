0ce2291994741c538be721be73314d200a975c8f
adilelmahi <adil.elm17@gmail.com>
2018-12-30 16:07:45 +0100

    Add VR-Interaction project

---
+++ VrInteraction/Assets/GameController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class GameController : MonoBehaviour {
+	public TextMesh infoText;
+	public Player player;
+	public GameObject enemyContainer;
+	private float restartTimer = 3f;
+
+	// Use this for initialization
+	void Start () {
+		infoText.text = "Find the button and \nenter the castle!";
+			
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.enteredCastle) {
+			int enemiesRemaining = enemyContainer.GetComponentsInChildren<Enemy> ().Length;
+			if (enemiesRemaining > 0) {
+				infoText.text = "Destroy all the enemies!";
+				infoText.text += "\nEnemies remaining:" + enemiesRemaining;
+			} else {
+				infoText.text = "you win";
+				restartTimer -= Time.deltaTime;
+
+				if (restartTimer <= 0f) {
+					UnityEngine.SceneManagement.SceneManager.LoadScene (UnityEngine.SceneManagement.SceneManager.GetActiveScene ().name);
+			}
+
+
+			}
+
+
+
+		}
+		
+	}
+}
---
+++ VrInteraction/Assets/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	
+	public float speed = 0.5f;
+	public Vector3 castlePosition;
+	private Vector3 targetPosition;
+	public bool enteredCastle;
+	
+
+	// Use this for initialization
+	void Start () {
+		targetPosition = transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		if(Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward),out hit))
+			{
+			if (hit.transform.GetComponent<DoorButton>() != null) {
+				hit.transform.GetComponent<DoorButton>().OnLook ();
+				MoveToCastle ();
+			}
+				
+			if (GvrViewer.Instance.Triggered || Input.GetKeyDown ("space")) {
+				RaycastHit enemyHit;
+				if(Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward),out enemyHit)){
+					if (hit.transform.GetComponent<Enemy>() != null) {
+						Destroy(hit.transform.gameObject);
+						
+
+
+
+					}
+
+
+
+				}
+			}
+			
+			}
+		transform.position = Vector3.Lerp (transform.position, targetPosition, Time.deltaTime * speed);
+
+
+
+			   	  }
+	public void MoveToCastle(){
+	
+		targetPosition = castlePosition;
+		enteredCastle = true;
+	}
+}
